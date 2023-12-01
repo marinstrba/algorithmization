5a92d40a6cb3703a31b578642a04299d69a4c528
marinstrba <marinstrba@gmail.com>
2023-12-01 21:29:25 +0100

    UPDATE: I have completed my java exercise.

---
+++ easy/removeDuplicate/Main.java
@@ -0,0 +1,27 @@
+class Solution {
+  public int removeDuplicates(int[] nums) {
+      
+    int     k;
+    boolean foundDuplicate;
+    int[]   nonDuplicateArray = new int[nums.length];
+
+    k = 0;
+    for (int i = 0; i < nums.length; ++i)
+    {
+      foundDuplicate = false;
+      for (int j = 0; j < k; ++j)
+      {
+        if (nums[i] == nonDuplicateArray[j])
+          foundDuplicate = true;
+      }
+      if (!foundDuplicate)
+      {
+        nonDuplicateArray[k] = nums[i];
+        k++;
+      }
+    }
+    for (int i = 0; i < k; ++i)
+      nums[i] = nonDuplicateArray[i];
+    return k;
+  }
+}
