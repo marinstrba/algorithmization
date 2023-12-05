863357aa97cef5135eace97d65b436e484531db6
marinstrba <marinstrba@gmail.com>
2023-12-05 12:35:19 +0100

    Adding a main class.

---
+++ hard/findMedianSortedArray/Main.java
@@ -0,0 +1,6 @@
+class Main{
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    solution.findMedianSortedArrays(new int[] {1, 2, 3}, new int[] {4, 5, 6});
+  }
+}
