1c6545c0f9a3065ee94e4d0e516ea98d772d6ee4
Marian Strba <marinstrba@gmail.com>
2023-12-06 15:40:42 +0100

    UPDATE

--- easy/searchInsertPosition/Main.java
+++ easy/searchInsertPosition/Main.java
@@ -1,19 +1,7 @@
-class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return low;
+public class Main{
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        solution.searchInsert(new int[]{2, 3, 6, 8}, 2);
     }
 }
